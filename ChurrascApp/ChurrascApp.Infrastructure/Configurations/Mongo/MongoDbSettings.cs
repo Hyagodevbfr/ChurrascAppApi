82b2da9f3be5b148eb52955d86d7a513d36f8342
Hyago Brito <hyago.sbrito@gmail.com>
2025-07-18 19:10:23 -0300

    feat: add MongoDbContext and MongoDbSettings classes for MongoDB configuration

---
+++ ChurrascApp/ChurrascApp.Infrastructure/Configurations/Mongo/MongoDbSettings.cs
@@ -0,0 +1,11 @@
+using ChurrascApp.Domain.Entities;
+
+namespace ChurrascApp.Infrastructure.Configurations;
+
+public class MongoDbSettings
+{
+    public string ConnectionString { get; set; } = string.Empty;
+    public string DatabaseName { get; set; } = string.Empty;
+    
+    public User Users {get; set;} 
+}

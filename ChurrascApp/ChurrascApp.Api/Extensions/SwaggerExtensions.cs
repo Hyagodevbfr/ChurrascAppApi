7b275a4cce9962d65a62d4915ebf1bdfa34ac3f2
Hyago Brito <hyago.sbrito@gmail.com>
2025-07-19 13:07:16 -0300

    feat: add Swagger extension for API documentation and security configuration

---
+++ ChurrascApp/ChurrascApp.Api/Extensions/SwaggerExtensions.cs
@@ -0,0 +1,36 @@
+using Microsoft.OpenApi.Models;
+
+namespace ChurrascApp.Api.Extensions;
+
+public static class SwaggerExtensions
+{
+    public static IServiceCollection AddSwagger(this IServiceCollection services)
+    {
+        services.AddSwaggerGen(c =>
+        {
+            c.SwaggerDoc("v1", new OpenApiInfo { Title = "ChurrascApp API", Version = "v1" });
+            c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+            {
+                Name = "Authorization",
+                Description = "Please enter a valid token",
+                In = ParameterLocation.Header,
+                Type = SecuritySchemeType.ApiKey
+            });
+            c.AddSecurityRequirement(new OpenApiSecurityRequirement
+            {
+                {
+                    new OpenApiSecurityScheme
+                    {
+                        Reference = new OpenApiReference
+                        {
+                            Type = ReferenceType.SecurityScheme,
+                            Id = "Bearer"
+                        }
+                    },
+                    new string[] {}
+                }
+            });
+        });
+        return services;
+    }
+}

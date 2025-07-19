ddc0f5325e2c085b6a4fead0ae8ff527f6605c60
Hyago Brito <hyago.sbrito@gmail.com>
2025-07-19 12:57:41 -0300

    refactor: remove TokenConfiguration class as it is no longer needed

--- ChurrascApp/ChurrascApp.Infrastructure/Configurations/Token/TokenConfiguration.cs
+++
@@ -1,9 +0,0 @@
-namespace ChurrascApp.Infrastructure.Configurations.Token;
-
-public class TokenConfiguration
-{
-    public string Audience { get; set; }
-    public string Issuer { get; set; }
-    public string Secret { get; set; }
-    public int TimeToExpiry { get; set; }
-}

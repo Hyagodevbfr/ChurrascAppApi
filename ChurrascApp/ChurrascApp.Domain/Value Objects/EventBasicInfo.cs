85d619eca9a2424f45d90f8a0b838d4d8c2fbe7f
Hyago Brito <hyago.sbrito@gmail.com>
2025-07-30 12:34:31 -0300

    feat: create event basic info value object

---
+++ ChurrascApp/ChurrascApp.Domain/Value Objects/EventBasicInfo.cs
@@ -0,0 +1,7 @@
+namespace ChurrascApp.Domain.Value_Objects;
+
+public class EventBasicInfo
+{
+    public string Name { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+}

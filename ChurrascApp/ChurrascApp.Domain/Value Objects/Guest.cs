3f91e22f46c6160915beee1c077f6a57ba066f7e
Hyago Brito <hyago.sbrito@gmail.com>
2025-07-30 12:35:25 -0300

    feat: create guest value object

---
+++ ChurrascApp/ChurrascApp.Domain/Value Objects/Guest.cs
@@ -0,0 +1,8 @@
+namespace ChurrascApp.Domain.Value_Objects;
+
+public class Guest
+{
+    public string Id { get; set; } = string.Empty;
+    public string Name { get; set; } = string.Empty;
+    public string PhoneNumber { get; set; } = string.Empty;
+}

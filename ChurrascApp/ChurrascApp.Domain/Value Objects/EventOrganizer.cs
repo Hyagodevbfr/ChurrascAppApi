22f3e01ad49dffa16d089662e8421d69ff794ae4
Hyago Brito <hyago.sbrito@gmail.com>
2025-07-30 12:34:48 -0300

    feat: create event organizer value object

---
+++ ChurrascApp/ChurrascApp.Domain/Value Objects/EventOrganizer.cs
@@ -0,0 +1,8 @@
+namespace ChurrascApp.Domain.Value_Objects;
+
+public class EventOrganizer
+{
+    public string Id { get; set; } = string.Empty;
+    public string Name { get; set; } = string.Empty;
+    public string Number { get; set; } =  string.Empty;
+}

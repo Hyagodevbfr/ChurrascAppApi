a1f33f7e12fa919e2446441bc80e4e8bedc6632f
Hyago Brito <hyago.sbrito@gmail.com>
2025-07-31 18:39:53 -0300

    refactor: add validations to date and time value object

--- ChurrascApp/ChurrascApp.Domain/Value Objects/DateAndTime.cs
+++ ChurrascApp/ChurrascApp.Domain/Value Objects/DateAndTime.cs
@@ -2,9 +2,9 @@ namespace ChurrascApp.Domain.Value_Objects;
 
 public class DateAndTime
 {
-    public DateOnly Date { get; set; }
-    public TimeOnly Time { get; set; }
-
+    public DateOnly Date { get; private set; }
+    public TimeOnly Time { get; private set; }
+    
     public DateAndTime(DateOnly date, TimeOnly time)
     {
         Validate(date, time);
@@ -16,15 +16,17 @@ public class DateAndTime
     {
         var today = new DateOnly(DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.Day);
         var now = new TimeOnly(DateTime.Now.Hour);
+        var sixMonthsBeforeToday = new DateOnly(DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.Day).AddMonths(6);
         
         if (date < today && time < now)
-            throw new ArgumentException("Date cannot be in the future");
+            throw new ArgumentException("Date must be after this exact moment.");
         
-        // Create validation if date is Greater than some year
+        if (date > sixMonthsBeforeToday)
+            throw new ArgumentException("Date must be within six months.");
     }
 
     public override string ToString()
     {
-        return $"{Date:dd/MM/yyyy} {Time:hh\\:mm}";
+        return $"{Date:dd/MM/yyyy} - {Time:hh\\:mm}";
     }
 }

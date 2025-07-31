14e8f46eae39b5b83c43d1e4d29f119bb255e2c5
Hyago Brito <hyago.sbrito@gmail.com>
2025-07-31 18:47:58 -0300

    feat: create interface and implementation to event repository

---
+++ ChurrascApp/ChurrascApp.Domain/Repositories/IEventRepository.cs
@@ -0,0 +1,8 @@
+﻿using ChurrascApp.Domain.Entities;
+
+namespace ChurrascApp.Domain.Repositories;
+
+public interface IEventRepository : IBaseRepository<Event>
+{
+    Task<Event> GetByInviteCode(string inviteCode);
+}
---
+++ ChurrascApp/ChurrascApp.Infrastructure/Persistence/EventRepository.cs
@@ -0,0 +1,29 @@
+﻿using ChurrascApp.Domain;
+using ChurrascApp.Domain.Entities;
+using ChurrascApp.Domain.Repositories;
+using ChurrascApp.Infrastructure.Configurations.Mongo;
+using Microsoft.Extensions.Options;
+using MongoDB.Driver;
+
+namespace ChurrascApp.Infrastructure.Persistence;
+
+public class EventRepository : BaseRepository<Event>, IEventRepository
+{
+    private readonly IMongoCollection<Event> _mongoContext;
+
+    public EventRepository(IOptions<MongoDbSettings> eventService) : base(eventService)
+    {
+        var mongoClient = new MongoClient(eventService.Value.ConnectionString);
+        var mongoDatabase = mongoClient.GetDatabase(eventService.Value.DatabaseName);
+        _mongoContext = mongoDatabase.GetCollection<Event>(nameof(Event));
+    }
+
+    public Task<Event> GetByInviteCode(string inviteCode)
+    {
+        var filter = Builders<Event>.Filter.Eq(e => e.InviteCode.Code, inviteCode);
+
+        var eventEntity = _mongoContext.Find(filter).FirstOrDefaultAsync();
+        return eventEntity;
+
+    }
+}

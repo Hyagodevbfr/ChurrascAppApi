e6190bf4ab09466e9f2c9a2ffa260a94a5aad0cd
Hyago Brito <hyago.sbrito@gmail.com>
2025-08-08 17:52:44 -0300

    feat: create EventUpdateRequest model for event updates

---
+++ ChurrascApp/ChurrascApp.Api/Models/Requests/EventUpdateRequest.cs
@@ -0,0 +1,27 @@
+﻿using ChurrascApp.Domain.Enums;
+using ChurrascApp.Domain.Value_Objects;
+
+namespace ChurrascApp.Api.Models.Requests;
+
+public class EventUpdateRequest
+{
+    public string Id { get; set; }
+    public EventBasicInfo BasicInfo { get; set; }
+    public EventOrganizer? Organizer { get; set; }
+    public bool HasExtraActivities { get; set; }
+    public ExtraActivity? ExtraActivity { get; set; }
+    public bool HasRequiredItems { get; set; }
+    public List<RequiredItem>? RequiredItems { get; set; }
+    public ContributionType ContributionType { get; set; }
+    public TotalCost TotalCost { get; set; }
+    public InviteCode InviteCode { get; set; }
+    public bool LimitedGuests { get; set; }
+    public int? NumberOfGuests { get; set; }
+    public List<Guest> InvitedGuests { get; set; }
+    public List<ConfirmedGuest> ConfirmedGuests { get; set; }
+
+    public EventUpdateRequest()
+    {
+        
+    }
+}

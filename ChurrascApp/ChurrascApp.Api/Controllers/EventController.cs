f956c71434e58d93998a1cb9660cdf955c0bb215
Hyago Brito <hyago.sbrito@gmail.com>
2025-08-08 17:53:11 -0300

    feat: add methods to EventController

--- ChurrascApp/ChurrascApp.Api/Controllers/EventController.cs
+++ ChurrascApp/ChurrascApp.Api/Controllers/EventController.cs
@@ -36,5 +36,82 @@ namespace ChurrascApp.Api.Controllers
             return Ok(response);
         }
 
+        [HttpGet("GetById/{id}")]
+        public async Task<IActionResult> GetById(string id)
+        {
+            var result = await _eventService.GetById(id);
+
+            var eventResponse = result.ToResponse();
+
+            var response = new ViewResponse<EventResponse>(
+                true,
+                "Event retrieved successfully",
+                eventResponse
+            );
+
+            return Ok(response);
+        }
+
+        [HttpGet("GetAll")]
+        public async Task<IActionResult> GetAll()
+        {
+            var result = await _eventService.GetAll();
+
+            var eventResponses = result.Select(e => e.ToResponse()).ToList();
+
+            var response = new ViewResponse<List<EventResponse>>(
+                true,
+                "Events retrieved successfully",
+                eventResponses
+            );
+
+            return Ok(response);
+        }
+
+        [HttpGet("GetByInviteCode/{inviteCode}")]
+        public async Task<IActionResult> GetByInviteCode(string inviteCode)
+        {
+            var result = await _eventService.GetByInviteCode(inviteCode);
+
+            var eventResponse = result.ToResponse();
+
+            var response = new ViewResponse<EventResponse>(
+                true,
+                "Event retrieved successfully",
+                eventResponse
+            );
+
+            return Ok(response);
+        }
+
+
+        [HttpPatch("Update")]
+        public async Task<IActionResult> Update([FromBody] EventUpdateRequest request)
+        {
+            var result = await _eventService.Update(request.ToDto());
+            var eventResponse = result.ToResponse();
+
+            var response = new ViewResponse<EventResponse>(
+                true,
+                "Event updated successfully",
+                eventResponse
+            );
+
+            return Ok(response);
+        }
+
+        [HttpDelete("Delete/{id}")]
+        public async Task<IActionResult> Delete(string id)
+        {
+            await _eventService.Delete(id);
+
+            var response = new ViewResponse<bool>(
+                true,
+                "Event deleted successfully",
+                true
+            );
+
+            return Ok(response);
+        }
     }
 }

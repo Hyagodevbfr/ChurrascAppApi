0e8f6435ba9c15c2cccaa4f57154b91665a32003
Hyago Brito <hyago.sbrito@gmail.com>
2025-08-11 16:50:36 -0300

    feat: implement JoinRequestCode value object for managing join request identifiers

---
+++ ChurrascApp/ChurrascApp.Domain/Value Objects/JoinRequestCode.cs
@@ -0,0 +1,20 @@
+﻿
+namespace ChurrascApp.Domain.Value_Objects;
+
+public class JoinRequestCode
+{
+    public string Code { get; set; }
+    public JoinRequestCode(string eventId, string userId)
+    {
+        Validate(eventId, userId);
+        Code = eventId + userId;
+    }
+
+    private void Validate(string eventId, string userId)
+    {
+        if (string.IsNullOrEmpty(eventId))
+            throw new ArgumentException("Event ID cannot be null or empty.", nameof(eventId));
+        if (string.IsNullOrEmpty(userId))
+            throw new ArgumentException("User ID cannot be null or empty.", nameof(userId));
+    }
+}

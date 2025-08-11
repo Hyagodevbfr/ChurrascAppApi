578ee8ae633316e110ccb60c4a2fed5366c8528b
Hyago Brito <hyago.sbrito@gmail.com>
2025-08-11 16:52:33 -0300

    feat: create join request extensions

---
+++ ChurrascApp/ChurrascApp.Application/Mappers/JoinRequestExtensions.cs
@@ -0,0 +1,30 @@
+﻿using ChurrascApp.Application.DTOs.JoinRequest;
+using ChurrascApp.Domain.Entities;
+
+namespace ChurrascApp.Application;
+
+public static class JoinRequestExtensions
+{
+    public static JoinRQResponseDto ToResponse(this JoinRequest joinRequest)
+    {
+        return new JoinRQResponseDto(
+            joinRequest.UserId,
+            joinRequest.EventId,
+            joinRequest.FullName,
+            joinRequest.PhoneNumber,
+            joinRequest.Status.ToString(),
+            joinRequest.CodeRequest.Code,
+            joinRequest.RequestedAt
+        );
+    }
+
+    public static JoinRequest ToEntity(this JoinRequestRegisterDto registerDto)
+    {
+        return new JoinRequest(
+            registerDto.UserId,
+            registerDto.EventId,
+            registerDto.FullName,
+            registerDto.PhoneNumber
+        );
+    }
+}

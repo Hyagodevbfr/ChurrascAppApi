1ac777c13fb0c1583dc3089fa20c94e0baf438f8
Hyago Brito <hyago.sbrito@gmail.com>
2025-08-11 17:00:04 -0300

    feat: add JoinRQResponse model for join request responses

---
+++ ChurrascApp/ChurrascApp.Api/Models/Responses/JoinRequest/JoinRQResponse.cs
@@ -0,0 +1,10 @@
+﻿namespace ChurrascApp.Api.Models.Responses.JoinRequest;
+
+public class JoinRQResponse
+{
+    public string UserId { get; set; } = string.Empty;
+    public string EventId { get; set; } = string.Empty;
+    public string FullName { get; set; } = string.Empty;
+    public string PhoneNumber { get; set; } = string.Empty;
+    public DateTime RequestedAt { get; set; }
+}

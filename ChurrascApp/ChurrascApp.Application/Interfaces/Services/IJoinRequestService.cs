cb4b66f018bbd767050193509a67e995450e4714
Hyago Brito <hyago.sbrito@gmail.com>
2025-08-11 16:59:18 -0300

    feat: create interface and implementation to join request service

---
+++ ChurrascApp/ChurrascApp.Application/Interfaces/Services/IJoinRequestService.cs
@@ -0,0 +1,11 @@
+﻿using ChurrascApp.Application.DTOs.JoinRequest;
+
+namespace ChurrascApp.Application.Interfaces.Services;
+
+public interface IJoinRequestService : IBaseService<JoinRQResponseDto, JoinRequestRegisterDto, JoinRequestUpdateDto>
+{
+    Task<JoinRQResponseDto> GetRequestByUser(string eventId, string userId);
+    Task<IList<JoinRQResponseDto>> GetAllRequestsByEvent(string eventId);
+    Task<IList<JoinRQResponseDto>> GetAllRequestsByUser(string userId);
+    Task<JoinRQResponseDto> RespondToRequest(string eventId, string userId, bool isAccepted);
+}
---
+++ ChurrascApp/ChurrascApp.Application/Services/JoinRequestService.cs
@@ -0,0 +1,71 @@
+﻿using ChurrascApp.Application.DTOs.JoinRequest;
+using ChurrascApp.Application.Interfaces.Services;
+using ChurrascApp.Domain.Repositories;
+
+namespace ChurrascApp.Application;
+
+public class JoinRequestService : IJoinRequestService
+{
+    private readonly IJoinRequestRepository _joinRequestRepository;
+
+    public JoinRequestService(IJoinRequestRepository joinRequestRepository)
+    {
+        _joinRequestRepository = joinRequestRepository;
+    }
+
+    public Task Delete(string id)
+    {
+        return _joinRequestRepository.Delete(id);
+    }
+
+    public Task<IEnumerable<JoinRQResponseDto>> GetAll()
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<IList<JoinRQResponseDto>> GetAllRequestsByEvent(string eventId)
+    {
+        var requests = await _joinRequestRepository.GetAllRequestsByEvent(eventId);
+
+        return requests.Select(r => r.ToResponse()).ToList();
+    }
+
+    public async Task<IList<JoinRQResponseDto>> GetAllRequestsByUser(string userId)
+    {
+        var requests = await _joinRequestRepository.GetAllRequestsByUser(userId);
+
+        return requests.Select(r => r.ToResponse()).ToList();
+    }
+
+    public Task<JoinRQResponseDto> GetById(string id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<JoinRQResponseDto> GetRequestByUser(string eventId, string userId)
+    {
+        var request = await _joinRequestRepository.GetRequestByUser(eventId, userId);
+
+        return request.ToResponse();
+    }
+
+    public async Task<JoinRQResponseDto> Register(JoinRequestRegisterDto registerDto)
+    {
+        var joinRequest = registerDto.ToEntity();
+        await _joinRequestRepository.CreateRequest(joinRequest);
+
+        return joinRequest.ToResponse();
+    }
+
+    public async Task<JoinRQResponseDto> RespondToRequest(string eventId, string userId, bool isAccepted)
+    {
+        var request = await _joinRequestRepository.RespondToRequest(eventId, userId, isAccepted);
+        
+        return request.ToResponse();
+    }
+
+    public Task<JoinRQResponseDto> Update(JoinRequestUpdateDto updateDto)
+    {
+        throw new NotImplementedException();
+    }
+}

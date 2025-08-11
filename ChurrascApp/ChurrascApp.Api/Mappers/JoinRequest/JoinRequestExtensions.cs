c20658f3ea670a5e97c7ad4682f0b39639d2588b
Hyago Brito <hyago.sbrito@gmail.com>
2025-08-11 17:00:16 -0300

    feat: add JoinRequestExtensions for mapping join request models

---
+++ ChurrascApp/ChurrascApp.Api/Mappers/JoinRequest/JoinRequestExtensions.cs
@@ -0,0 +1,40 @@
+﻿using ChurrascApp.Api.Models.Requests;
+using ChurrascApp.Application.DTOs.JoinRequest;
+using ChurrascApp.Api.Models.Responses.JoinRequest;
+using ChurrascApp.Domain.Entities;
+
+namespace ChurrascApp.Api;
+
+public static class JoinRequestExtensions
+{
+    public static JoinRequest ToResponse(this JoinRQResponse joinRequest)
+    {
+        return new JoinRequest(
+            userId: joinRequest.UserId,
+            eventId: joinRequest.EventId,
+            fullName: joinRequest.FullName,
+            phoneNumber: joinRequest.PhoneNumber
+        );
+    }
+
+    public static JoinRequestRegisterDto ToDto(this JoinRegisterRequest request)
+    {
+        return new JoinRequestRegisterDto(
+            request.UserId,
+            request.EventId,
+            request.FullName,
+            request.PhoneNumber
+        );
+    }
+
+    public static JoinRQResponse ToResponse(this JoinRQResponseDto dto)
+    {
+        return new JoinRQResponse{
+            UserId = dto.UserId,
+            EventId = dto.EventId,
+            FullName = dto.FullName,
+            PhoneNumber = dto.PhoneNumber,
+            RequestedAt = dto.RequestedAt
+        };
+    }
+}

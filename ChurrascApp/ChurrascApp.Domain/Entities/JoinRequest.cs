c188df6be286965789af7039710c013d7f455151
Hyago Brito <hyago.sbrito@gmail.com>
2025-08-11 16:51:14 -0300

    feat: create join request model

---
+++ ChurrascApp/ChurrascApp.Domain/Entities/JoinRequest.cs
@@ -0,0 +1,42 @@
+﻿using ChurrascApp.Domain.Enums;
+using ChurrascApp.Domain.Value_Objects;
+
+namespace ChurrascApp.Domain.Entities;
+
+public class JoinRequest : BaseEntity
+{
+    public string UserId { get; set; }
+    public string EventId { get; set; }
+    public string FullName { get; set; }
+    public string PhoneNumber { get; set; }
+    public StatusJoinRequest Status { get; set; }
+    public JoinRequestCode CodeRequest { get; set; }
+    public DateTime RequestedAt { get; set; }
+
+    public JoinRequest() { }
+
+    public JoinRequest(string userId, string eventId, string fullName, string phoneNumber)
+    {
+        Validate(userId, eventId, fullName, phoneNumber);
+
+        UserId = userId;
+        EventId = eventId;
+        FullName = fullName;
+        PhoneNumber = phoneNumber;
+        Status = StatusJoinRequest.Pending;
+        CodeRequest = new JoinRequestCode(eventId, userId);
+        RequestedAt = DateTime.UtcNow;
+    }
+
+    private void Validate(string userId, string eventId, string fullName, string phoneNumber)
+    {
+        if (string.IsNullOrEmpty(userId))
+            throw new ArgumentException("User ID cannot be null or empty.", nameof(userId));
+        if (string.IsNullOrEmpty(eventId))
+            throw new ArgumentException("Event ID cannot be null or empty.", nameof(eventId));
+        if (string.IsNullOrEmpty(fullName))
+            throw new ArgumentException("Full name cannot be null or empty.", nameof(fullName));
+        if (string.IsNullOrEmpty(phoneNumber))
+            throw new ArgumentException("Phone number cannot be null or empty.", nameof(phoneNumber));
+    }
+}

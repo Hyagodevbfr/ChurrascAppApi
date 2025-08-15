ce6fce87d6b8c281562ccc16beb119df576bfccc
Hyago Brito <hyago.sbrito@gmail.com>
2025-08-15 20:30:15 -0300

    feat: create interface and implementation to participant repository

---
+++ ChurrascApp/ChurrascApp.Domain/Repositories/IParticipantRepository.cs
@@ -0,0 +1,15 @@
+﻿using ChurrascApp.Domain.Entities;
+
+namespace ChurrascApp.Domain.Repositories;
+
+public interface IParticipantRepository : IBaseRepository<Participant>
+{
+    Task<Participant> GetParticipantByIdFromEvent(string userId, string eventId);
+    Task<IList<Participant>> GetParticipantsByEventId(string eventId);
+    Task<IList<Participant>> GetConfirmedParticipantsByEventId(string eventId);
+    Task<IList<Participant>> GetAcceptedAndPendentParticipantsByEventId(string eventId);
+    Task<Participant> CancelParticipation(string userId);
+    Task<Participant> SolicitParticipation(object request, User user, Event eventEntity);
+    Task<Participant> ConfirmParticipant(string userId, bool isConfirmed);
+    Task<Participant> ConfirmPayment(string userId);
+}
---
+++ ChurrascApp/ChurrascApp.Infrastructure/Persistence/ParticipantRepository.cs
@@ -0,0 +1,144 @@
+﻿using ChurrascApp.Application.DTOs.Participant;
+using ChurrascApp.Domain.Entities;
+using ChurrascApp.Domain.Enums;
+using ChurrascApp.Domain.Repositories;
+using ChurrascApp.Infrastructure.Configurations.Mongo;
+using ChurrascApp.Infrastructure.Persistence;
+using Microsoft.Extensions.Options;
+using MongoDB.Driver;
+
+namespace ChurrascApp.Infrastructure;
+
+public class ParticipantRepository : BaseRepository<Participant>, IParticipantRepository
+{
+    private readonly IMongoCollection<Participant> _mongoContext;
+    private readonly IUserRepository _userRepository;
+    private readonly IEventRepository _eventRepository;
+    public ParticipantRepository(IOptions<MongoDbSettings> services, IUserRepository userRepository, IEventRepository eventRepository) : base(services)
+    {
+        var mongoClient = new MongoClient(services.Value.ConnectionString);
+        var mongoDatabase = mongoClient.GetDatabase(services.Value.DatabaseName);
+        _mongoContext = mongoDatabase.GetCollection<Participant>(nameof(Participant));
+        _userRepository = userRepository;
+        _eventRepository = eventRepository;
+    }
+
+    public async Task<IList<Participant>> GetConfirmedParticipantsByEventId(string eventId)
+    {
+        if (string.IsNullOrEmpty(eventId))
+            throw new ArgumentException("Event ID cannot be null or empty.");
+
+        var filter = Builders<Participant>.Filter.And(
+            Builders<Participant>.Filter.Eq(p => p.EventId, eventId),
+            Builders<Participant>.Filter.Eq(p => p.Status, StatusParticipant.Confirmed)
+        );
+
+        return await _mongoContext.Find(filter).ToListAsync();
+    }
+    public async Task<IList<Participant>> GetAcceptedAndPendentParticipantsByEventId(string eventId)
+    {
+        if (string.IsNullOrEmpty(eventId))
+            throw new ArgumentException("Event ID cannot be null or empty.");
+
+        var filter = Builders<Participant>.Filter.And(
+            Builders<Participant>.Filter.Eq(p => p.EventId, eventId),
+            Builders<Participant>.Filter.Eq(p => p.Status, StatusParticipant.Accepted | StatusParticipant.Pending)
+        );
+
+        return await _mongoContext.Find(filter).ToListAsync();
+    }
+    public async Task<Participant> GetParticipantByIdFromEvent(string userId, string eventId)
+    {
+        var filter = Builders<Participant>.Filter.And(
+            Builders<Participant>.Filter.Eq(p => p.UserId, userId),
+            Builders<Participant>.Filter.Eq(p => p.EventId, eventId)
+        );
+
+        return await _mongoContext.Find(filter).FirstOrDefaultAsync();
+    }
+
+    public async Task<IList<Participant>> GetParticipantsByEventId(string eventId)
+    {
+        if (string.IsNullOrEmpty(eventId))
+            throw new ArgumentException("Event ID cannot be null or empty.");
+
+        var filter = Builders<Participant>.Filter.Eq(p => p.EventId, eventId);
+
+        return await _mongoContext.Find(filter).ToListAsync();
+    }
+
+    public async Task<Participant> SolicitParticipation(object request, User user, Event eventEntity)
+    {
+        if (request is not ParticipationRequestDto participationRequest)
+            throw new ArgumentException("Invalid request type.");
+
+        ValidateParticipationRequest(participationRequest, eventEntity);
+
+        ValidateRequestsValues(participationRequest, eventEntity);
+
+        var filterParticipant = Builders<Participant>.Filter.Eq(
+                                                    p => p.UserId, user.Id);
+
+        var participant = await _mongoContext.Find(filterParticipant).
+                                                    FirstOrDefaultAsync();
+
+        participant.UpdateStatus(StatusParticipant.Pending);
+
+        await _mongoContext.ReplaceOneAsync(
+            Builders<Participant>.Filter.Eq(p => p.Id, participant.Id),
+            participant
+        );
+
+        return participant;
+    }
+
+    public Task<Participant> ConfirmParticipant(string userId, bool isConfirmed)
+    {
+        var filter = Builders<Participant>.Filter.Eq(p => p.UserId, userId);
+        var update = Builders<Participant>.Update.Set(p => p.Status, isConfirmed ? StatusParticipant.Accepted : StatusParticipant.Rejected)
+                                                  .Set(p => p.UpdatedStatusAt, DateTime.UtcNow);
+
+        return _mongoContext.FindOneAndUpdateAsync(filter, update);
+
+    }
+
+    public Task<Participant> CancelParticipation(string userId)
+    {
+        var filter = Builders<Participant>.Filter.Eq(p => p.UserId, userId);
+        var update = Builders<Participant>.Update.Set(p => p.Status, StatusParticipant.Cancelled)
+                                                  .Set(p => p.UpdatedStatusAt, DateTime.UtcNow);
+
+        return _mongoContext.FindOneAndUpdateAsync(filter, update);
+    }
+    public async Task<Participant> ConfirmPayment(string userId)
+    {
+        
+        var filter = Builders<Participant>.Filter.Eq(p => p.UserId, userId);
+        var update = Builders<Participant>.Update.Set(p => p.ContributedAmount.IsPaid, true)
+                                                  .Set(p => p.UpdatedStatusAt, DateTime.UtcNow);
+
+        return await _mongoContext.FindOneAndUpdateAsync(filter, update);
+    }
+
+
+    // Validates the participation request
+    private void ValidateParticipationRequest(ParticipationRequestDto request, Event eventEntity)
+    {
+        if (eventEntity.HasExtraActivities && request.ParticipantInExtraActivity is null)
+            throw new ArgumentException("Participant must specify if they will join the extra activity.");
+
+        if (eventEntity.HasRequiredItems && request.AssignedItems is null)
+            throw new ArgumentException("Participant must specify required items if applicable.");
+    }
+
+    public void ValidateRequestsValues(ParticipationRequestDto request, Event eventEntity)
+    {
+        if (eventEntity.HasExtraActivities && request.ParticipantInExtraActivity is null)
+            throw new ArgumentException("Participant must specify if they will join the extra activity.");
+
+        if (eventEntity.HasRequiredItems && request.AssignedItems is null)
+            throw new ArgumentException("Participant must specify required items if applicable.");
+    }
+
+    
+}

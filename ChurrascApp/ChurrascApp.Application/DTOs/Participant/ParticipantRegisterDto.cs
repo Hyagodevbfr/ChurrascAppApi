900efe1797d70e5a26920104abc0f03254969f37
Hyago Brito <hyago.sbrito@gmail.com>
2025-08-15 20:25:33 -0300

    feat: create participant register dto

---
+++ ChurrascApp/ChurrascApp.Application/DTOs/Participant/ParticipantRegisterDto.cs
@@ -0,0 +1,13 @@
+﻿using ChurrascApp.Domain.Value_Objects;
+
+namespace ChurrascApp.Application.DTOs.Participant;
+
+public record ParticipantRegisterDto(
+    string UserId,
+    string EventId,
+    string FullName,
+    string PhoneNumber,
+    List<AssignedItem>? AssignedItems = null,
+    ContributedAmount? ContributedAmount = null,
+    bool? ParticipantInExtraActivity = null
+);

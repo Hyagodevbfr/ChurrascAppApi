ab1d9b5450ecbac7d7ccef186ca4e0e2f6c7620b
Hyago Brito <hyago.sbrito@gmail.com>
2025-08-15 20:29:28 -0300

    feat: create contributed amount value object

---
+++ ChurrascApp/ChurrascApp.Domain/Value Objects/ContributedAmount.cs
@@ -0,0 +1,21 @@
+﻿namespace ChurrascApp.Domain.Value_Objects;
+
+public class ContributedAmount
+{
+    public decimal Value { get; private set; }
+    public bool IsPaid { get; private set; }
+    public ContributedAmount(decimal value, bool isPaid)
+    {
+        if (value < 0)
+            throw new ArgumentException("Contributed amount cannot be negative.", nameof(value));
+
+        Value = value;
+        IsPaid = isPaid;
+    }
+    
+    public void MarkAsPaid()
+    {
+        IsPaid = true;
+    }
+
+}

feda81f5328032ff848a9060d439970877f63f5e
Hyago Brito <hyago.sbrito@gmail.com>
2025-08-15 20:25:50 -0300

    feat: create participant response dto

---
+++ ChurrascApp/ChurrascApp.Application/DTOs/Participant/ParticipantResponseDto.cs
@@ -0,0 +1,16 @@
+﻿using ChurrascApp.Domain.Enums;
+using ChurrascApp.Domain.Value_Objects;
+
+namespace ChurrascApp.Application.DTOs.Participant;
+
+public record ParticipantResponseDto(
+    string UserId,
+    string EventId,
+    string FullName,
+    string PhoneNumber,
+    List<AssignedItem>? AssignedItems = null,
+    ContributedAmount? ContributedAmount = null,
+    bool? ParticipantInExtraActivity = null,
+    StatusParticipant Status = StatusParticipant.Accepted,
+    DateTime? UpdatedStatusAt = null
+);

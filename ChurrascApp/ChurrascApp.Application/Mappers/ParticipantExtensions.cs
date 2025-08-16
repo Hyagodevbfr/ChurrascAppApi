0007590d5bf556a5ac446647a07faed9ba8bd756
Hyago Brito <hyago.sbrito@gmail.com>
2025-08-16 19:42:02 -0300

    feat: create participant Extensions

---
+++ ChurrascApp/ChurrascApp.Application/Mappers/ParticipantExtensions.cs
@@ -0,0 +1,35 @@
+﻿using ChurrascApp.Application.DTOs.Participant;
+using ChurrascApp.Domain.Entities;
+
+namespace ChurrascApp.Application.Mappers;
+
+public static class ParticipantExtensions
+{
+    public static ParticipantResponseDto ToResponse(this Participant participant)
+    {
+        return new ParticipantResponseDto(
+            participant.UserId,
+            participant.EventId,
+            participant.FullName,
+            participant.PhoneNumber,
+            participant.AssignedItems,
+            participant.ContributedAmount,
+            participant.ParticipantInExtraActivity,
+            participant.Status,
+            participant.UpdatedStatusAt
+        );
+    }
+
+    public static Participant ToEntity(this ParticipantResponseDto responseDto)
+    {
+        return new Participant(
+            responseDto.UserId,
+            responseDto.EventId,
+            responseDto.FullName,
+            responseDto.PhoneNumber,
+            responseDto.AssignedItems,
+            responseDto.ContributedAmount,
+            responseDto.ParticipantInExtraActivity
+        );
+    }
+}

39d8fa94d2f41898597a886d59ea339104dc430b
Hyago Brito <hyago.sbrito@gmail.com>
2025-08-16 19:39:17 -0300

    feat: update SolicitParticipation method signature to use string userId and object eventEntity

--- ChurrascApp/ChurrascApp.Domain/Repositories/IParticipantRepository.cs
+++ ChurrascApp/ChurrascApp.Domain/Repositories/IParticipantRepository.cs
@@ -9,7 +9,7 @@ public interface IParticipantRepository : IBaseRepository<Participant>
     Task<IList<Participant>> GetConfirmedParticipantsByEventId(string eventId);
     Task<IList<Participant>> GetAcceptedAndPendentParticipantsByEventId(string eventId);
     Task<Participant> CancelParticipation(string userId);
-    Task<Participant> SolicitParticipation(object request, User user, Event eventEntity);
+    Task<Participant> SolicitParticipation(object request, string userId, object eventEntity);
     Task<Participant> ConfirmParticipant(string userId, bool isConfirmed);
     Task<Participant> ConfirmPayment(string userId);
 }

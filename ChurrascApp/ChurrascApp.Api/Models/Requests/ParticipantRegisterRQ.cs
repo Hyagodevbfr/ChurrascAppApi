5c36b109d4a46e25e4d8fde82f9691da20c2628f
Hyago Brito <hyago.sbrito@gmail.com>
2025-08-16 19:43:16 -0300

    feat: add participant register request model

---
+++ ChurrascApp/ChurrascApp.Api/Models/Requests/ParticipantRegisterRQ.cs
@@ -0,0 +1,14 @@
+﻿using ChurrascApp.Domain.Value_Objects;
+
+namespace ChurrascApp.Api.Models.Requests;
+
+public class ParticipantRegisterRQ
+{
+    public string UserId { get; set; } = string.Empty;
+    public string EventId { get; set; } = string.Empty;
+    public string FullName { get; set; } = string.Empty;
+    public string PhoneNumber { get; set; } = string.Empty;
+    public List<AssignedItem>? AssignedItems { get; set; }
+    public ContributedAmount? ContributedAmount { get; set; }
+    public bool? ParticipantInExtraActivity { get; set; }
+}

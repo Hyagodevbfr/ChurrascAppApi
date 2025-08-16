aa54b4aece4e225955797053beaabbd29033f842
Hyago Brito <hyago.sbrito@gmail.com>
2025-08-16 19:41:39 -0300

    feat: create interface and implementation to participant service

---
+++ ChurrascApp/ChurrascApp.Application/Interfaces/Services/IParticipantService.cs
@@ -0,0 +1,17 @@
+﻿using ChurrascApp.Application.DTOs.Event;
+using ChurrascApp.Application.DTOs.Participant;
+using ChurrascApp.Application.DTOs.User;
+
+namespace ChurrascApp.Application.Interfaces.Services;
+
+public interface IParticipantService : IBaseService<ParticipantResponseDto, ParticipantRegisterDto, ParticipantUpdateDto>
+{
+    Task<ParticipantResponseDto> GetParticipantByIdFromEvent(string userId, string eventId);
+    Task<IList<ParticipantResponseDto>> GetParticipantsByEventId(string eventId);
+    Task<IList<ParticipantResponseDto>> GetConfirmedParticipantsByEventId(string eventId);
+    Task<IList<ParticipantResponseDto>> GetAcceptedAndPendentParticipantsByEventId(string eventId);
+    Task<ParticipantResponseDto> CancelParticipation(string userId);
+    Task<ParticipantResponseDto> SolicitParticipation(ParticipationRequestDto request, string userId, EventResponseDto eventEntity);
+    Task<ParticipantResponseDto> ConfirmParticipant(string userId, bool isConfirmed);
+    Task<ParticipantRegisterDto> ConfirmPayment(string userId);
+}
---
+++ ChurrascApp/ChurrascApp.Application/Services/ParticipantService.cs
@@ -0,0 +1,94 @@
+﻿using ChurrascApp.Application.DTOs.Event;
+using ChurrascApp.Application.DTOs.Participant;
+using ChurrascApp.Application.DTOs.User;
+using ChurrascApp.Application.Interfaces.Services;
+using ChurrascApp.Application.Mappers;
+using ChurrascApp.Domain.Repositories;
+
+namespace ChurrascApp.Application.Services;
+
+public class ParticipantService : IParticipantService
+{
+    private readonly IParticipantRepository _participantRepository;
+    public ParticipantService(IParticipantRepository participantRepository)
+    {
+        _participantRepository = participantRepository;
+    }
+
+    // Main Methods
+    public async Task<IList<ParticipantResponseDto>> GetConfirmedParticipantsByEventId(string eventId)
+    {
+        var participants = await _participantRepository.GetConfirmedParticipantsByEventId(eventId);
+
+        return participants.Select(p => p.ToResponse()).ToList();
+    }
+    public async Task<IList<ParticipantResponseDto>> GetAcceptedAndPendentParticipantsByEventId(string eventId)
+    {
+        var participants = await _participantRepository.GetAcceptedAndPendentParticipantsByEventId(eventId);
+
+        return participants.Select(p => p.ToResponse()).ToList();
+    }
+    public async Task<ParticipantResponseDto> GetParticipantByIdFromEvent(string userId, string eventId)
+    {
+        var participant = await _participantRepository.GetParticipantByIdFromEvent(userId, eventId);
+
+        return participant.ToResponse();
+    }
+    public async Task<IList<ParticipantResponseDto>> GetParticipantsByEventId(string eventId)
+    {
+        var participants = await _participantRepository.GetParticipantsByEventId(eventId);
+
+        return participants.Select(p => p.ToResponse()).ToList();
+    }
+    public async Task<ParticipantResponseDto> SolicitParticipation(ParticipationRequestDto request, string userId, EventResponseDto eventEntity)
+    {
+        var requestParticipation = await _participantRepository.SolicitParticipation(request, userId, eventEntity);
+
+        return requestParticipation.ToResponse();
+    }
+    public async Task<ParticipantResponseDto> ConfirmParticipant(string userId, bool isConfirmed)
+    {
+        var participantConfirmed = await _participantRepository.ConfirmParticipant(userId, isConfirmed);
+
+        return participantConfirmed.ToResponse();
+    }
+    public async Task<ParticipantResponseDto> CancelParticipation(string userId)
+    {
+        var participantCanceled = await _participantRepository.CancelParticipation(userId);
+
+        return participantCanceled.ToResponse();
+    }
+    public async Task<ParticipantResponseDto> ConfirmPayment(string userId)
+    {
+        var confirmedParticipantPayment = await _participantRepository.ConfirmPayment(userId);
+
+        return confirmedParticipantPayment.ToResponse();
+    }
+    
+    // Generic Methods
+    public Task Delete(string id)
+    {
+        throw new NotImplementedException();
+    }
+    public Task<IEnumerable<ParticipantResponseDto>> GetAll()
+    {
+        throw new NotImplementedException();
+    }
+    public Task<ParticipantResponseDto> GetById(string id)
+    {
+        throw new NotImplementedException();
+    }
+    public Task<ParticipantResponseDto> Register(ParticipantRegisterDto registerDto)
+    {
+        throw new NotImplementedException();
+    }
+    public Task<ParticipantResponseDto> Update(ParticipantUpdateDto updateDto)
+    {
+        throw new NotImplementedException();
+    }
+
+    Task<ParticipantRegisterDto> IParticipantService.ConfirmPayment(string userId)
+    {
+        throw new NotImplementedException();
+    }
+}

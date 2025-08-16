44c8257868e1be54b53301b4124ab7d593b8727d
Hyago Brito <hyago.sbrito@gmail.com>
2025-08-16 19:42:19 -0300

    feat: add ParticipantRS response model

---
+++ ChurrascApp/ChurrascApp.Api/Models/Responses/Participant/ParticipantRS.cs
@@ -0,0 +1,17 @@
+﻿using ChurrascApp.Domain.Enums;
+using ChurrascApp.Domain.Value_Objects;
+
+namespace ChurrascApp.Api.Models.Responses.Participant;
+
+public class ParticipantRS
+{
+    public string UserId { get; set; } = string.Empty;
+    public string EventId { get; set; } = string.Empty;
+    public string FullName { get; set; } = string.Empty;
+    public string PhoneNumber { get; set; } = string.Empty;
+    public List<AssignedItem>? AssignedItems { get; set; }
+    public ContributedAmount? ContributedAmount { get; set; }
+    public bool? ParticipantInExtraActivity { get; set; }
+    public StatusParticipant Status { get; set; }
+    public DateTime? UpdatedStatusAt { get; set; }
+}

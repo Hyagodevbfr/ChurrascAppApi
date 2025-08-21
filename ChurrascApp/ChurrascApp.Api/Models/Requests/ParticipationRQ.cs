cb4960f52e99b380231af643e5147779643963e3
Hyago Brito <hyago.sbrito@gmail.com>
2025-08-21 17:44:54 -0300

    feat: add properties to ParticipationRQ for user and event details

--- ChurrascApp/ChurrascApp.Api/Models/Requests/ParticipationRQ.cs
+++ ChurrascApp/ChurrascApp.Api/Models/Requests/ParticipationRQ.cs
@@ -1,6 +1,12 @@
-﻿namespace ChurrascApp.Api.Models.Requests;
+﻿using ChurrascApp.Domain.Value_Objects;
+
+namespace ChurrascApp.Api.Models.Requests;
 
 public class ParticipationRQ
 {
-
+    public string UserId { get; set; } = string.Empty;
+    public string EventId { get; set; } = string.Empty;
+    public List<AssignedItem>? AssignedItems { get; set; }
+    public ContributedAmount? ContributedAmount { get; set; }
+    public bool? ParticipantInExtraActivity { get; set; }
 }

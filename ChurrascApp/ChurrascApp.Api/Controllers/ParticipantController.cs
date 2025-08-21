88aa833af77f672701ae407907ceb9fb4fdbc40a
Hyago Brito <hyago.sbrito@gmail.com>
2025-08-21 17:45:57 -0300

    feat: add new methods to participant controller

--- ChurrascApp/ChurrascApp.Api/Controllers/ParticipantController.cs
+++ ChurrascApp/ChurrascApp.Api/Controllers/ParticipantController.cs
@@ -1,6 +1,9 @@
+using System.Data.OleDb;
 using ChurrascApp.Api.Mappers;
+using ChurrascApp.Api.Mappers.Event;
 using ChurrascApp.Api.Models.Requests;
 using ChurrascApp.Api.Models.Responses;
+using ChurrascApp.Api.Models.Responses.Event;
 using ChurrascApp.Api.Models.Responses.Participant;
 using ChurrascApp.Application.DTOs.Participant;
 using ChurrascApp.Application.Interfaces.Services;
@@ -14,9 +17,11 @@ namespace ChurrascApp.Api.Controllers
     public class ParticipantController : BaseController
     {
         private readonly IParticipantService _participantService;
-        public ParticipantController(IParticipantService participantService)
+        private readonly IEventService _eventService;
+        public ParticipantController(IParticipantService participantService, IEventService eventService)
         {
             _participantService = participantService;
+            _eventService = eventService;
         }
 
         [HttpPost]
@@ -29,6 +34,127 @@ namespace ChurrascApp.Api.Controllers
                     true,
                     "Participant registered Successfully",
                     result.ToResponse()
+                )
+            );
+        }
+
+        [HttpGet("ConfirmedsByEventId/{id}")]
+        public async Task<IActionResult> GetConfirmedParticipantsByEventId(string eventId)
+        {
+            var result = await _participantService.GetConfirmedParticipantsByEventId(eventId);
+            var response = result.Select(p => p.ToResponse()).ToList();
+
+            return Ok(
+                new ViewResponse<IList<ParticipantRS>>(
+                    true,
+                    "Confirmed Participants Retreived Successfully",
+                    response
+                )
+            );
+        }
+        [HttpPatch("SolicitParticipation")]
+        public async Task<IActionResult> SolicitParticipation(ParticipationRQ participation)
+        {
+            var eventResponse = await _eventService.GetById(participation.EventId);
+
+            var result = await _participantService.SolicitParticipation(participation.ToDto(), participation.UserId, eventResponse);
+            var response = result.ToResponse();
+
+            return Ok(
+                new ViewResponse<ParticipantRS>(
+                    true,
+                    "Solicitation Created Successfully.",
+                    response
+                )
+            );
+        }
+
+        [HttpPatch("ConfirmParticipant")]
+        public async Task<IActionResult> ConfirmParticipant(string userId, bool isConfirmed)
+        {
+            var result = await _participantService.ConfirmParticipant(userId, isConfirmed);
+            var response = result.ToResponse();
+
+            return Ok(
+                new ViewResponse<ParticipantRS>(
+                    true,
+                    "Participant Updated Successfully."
+                )
+            );
+        }
+
+        [HttpPatch("CancelParticipation")]
+        public async Task<IActionResult> CancelParticipation(string userId)
+        {
+            var result = await _participantService.CancelParticipation(userId);
+            var response = result.ToResponse();
+
+            return Ok(
+                new ViewResponse<ParticipantRS>(
+                    true,
+                    "Participation Canceled Successfully.",
+                    response
+                )
+            );
+        }
+
+        [HttpPatch("ConfirmPayment")]
+        public async Task<IActionResult> ConfirmPayment(string userId)
+        {
+            var result = await _participantService.ConfirmPayment(userId);
+            var response = result.ToResponse();
+
+            return Ok(
+                new ViewResponse<ParticipantRS>(
+                    true,
+                    "Participant Payment Confirmed Successfully.",
+                    response
+                )
+            );
+        }
+
+        [HttpGet("AcceptedAndPendentByEventId/{eventId}")]
+        public async Task<IActionResult> AcceptedAndPendentByEventId(string eventId)
+        {
+            var result = await _participantService.GetAcceptedAndPendentParticipantsByEventId(eventId);
+            var response = result.Select(p => p.ToResponse()).ToList();
+
+            return Ok(
+                new ViewResponse<IList<ParticipantRS>>(
+                    true,
+                    "Accepted and Pendent Participants by Event Id",
+                    response
+                )
+            );
+        }
+
+        [HttpGet("GetParticipantByIdFromEvent")]
+        public async Task<IActionResult> GetParticipantByIdFromEvent(string userId, string eventId)
+        {
+            var result = await _participantService.GetParticipantByIdFromEvent(userId, eventId);
+
+            var response = result.ToResponse();
+
+            return Ok(
+                new ViewResponse<ParticipantRS>(
+                    true,
+                    "Participant Retreived Successfully.",
+                    response
+                )
+            );
+        }
+
+        [HttpGet("GetParticipantsByEventId/{eventId}")]
+        public async Task<IActionResult> GetParticipantsByEventId(string eventId)
+        {
+            var result = await _participantService.GetParticipantsByEventId(eventId);
+            var response = result.Select(p => p.ToResponse()).ToList();
+
+            return Ok(
+                new ViewResponse<IList<ParticipantRS>>(
+                    true,
+                    "Participants Retreived Successfully.",
+                    response
                 )
             );
         }

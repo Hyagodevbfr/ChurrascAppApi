3726bc2af2478705ef6b7d8527d249a4bf9a3c1a
Hyago Brito <hyago.sbrito@gmail.com>
2025-08-21 17:44:46 -0300

    feat: add methods for updating JoinRequest and converting to ParticipantRegisterDto

--- ChurrascApp/ChurrascApp.Application/Mappers/JoinRequestExtensions.cs
+++ ChurrascApp/ChurrascApp.Application/Mappers/JoinRequestExtensions.cs
@@ -1,5 +1,7 @@
 ﻿using ChurrascApp.Application.DTOs.JoinRequest;
+using ChurrascApp.Application.DTOs.Participant;
 using ChurrascApp.Domain.Entities;
+using ChurrascApp.Domain.Enums;
 
 namespace ChurrascApp.Application;
 
@@ -25,6 +27,52 @@ public static class JoinRequestExtensions
             registerDto.EventId,
             registerDto.FullName,
             registerDto.PhoneNumber
+        );
+    }
+
+    public static JoinRequestUpdateDto ToUpdate(this JoinRQResponseDto join)
+    {
+        return new JoinRequestUpdateDto(
+            join.UserId,
+            join.EventId,
+            join.FullName,
+            join.PhoneNumber,
+            join.Status,
+            join.CodeRequest
+        );
+    }
+
+    public static JoinRequestUpdateDto ToUpdateStatus(this JoinRQResponseDto join, string statusJoin)
+    {
+        return new JoinRequestUpdateDto(
+            join.UserId,
+            join.EventId,
+            join.FullName,
+            join.PhoneNumber,
+            statusJoin,
+            join.CodeRequest
+        );
+    }
+
+    public static JoinRequest ToEntity(this JoinRequestUpdateDto joinUpdate)
+    {
+        return new JoinRequest(
+            joinUpdate.UserId,
+            joinUpdate.EventId,
+            joinUpdate.FullName,
+            joinUpdate.PhoneNumber,
+            joinUpdate.CodeRequest
+        );
+    }
+
+    // Join to Participant
+    public static ParticipantRegisterDto ToParticipantRegister(this JoinRequest joinRequest)
+    {
+        return new ParticipantRegisterDto(
+            joinRequest.UserId,
+            joinRequest.EventId,
+            joinRequest.FullName,
+            joinRequest.PhoneNumber
         );
     }
 }
